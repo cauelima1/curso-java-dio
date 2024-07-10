2c7be26570f6292d2c250ff4372499b763d38469
caue <cauezak@gmail.com>
2024-07-10 15:10:26 -0300

    alteracoes

---
+++ src/edu/caue/segundasemana/Calendario.java
@@ -0,0 +1,14 @@
+package edu.caue.segundasemana;
+
+import java.util.Calendar;
+
+
+public class Calendario {
+
+    public static void main(String[] args) {
+        Calendar rightNow = Calendar.getInstance();
+        System.out.println(rightNow.getTime());
+    }
+
+    
+}
