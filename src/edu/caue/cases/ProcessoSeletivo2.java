8d4f372e218af23e9440b30901118123018a9610
cauelima1 <cauezak@gmail.com>
2024-07-14 01:15:57 -0300

    case02 e testes com array double

---
+++ src/edu/caue/cases/ProcessoSeletivo2.java
@@ -0,0 +1,36 @@
+package edu.caue.cases;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+
+public class ProcessoSeletivo2 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        double [] salarioPretendido = new double[10];
+      
+        
+        System.out.println("Temos " + candidatos.length + " candidatos:");
+       
+        
+        for (int i=0; i <=9; i++) {
+            System.out.println(i+1 + " - " + candidatos[i]);
+            salarioPretendido[i] = valorPretendido();
+            System.out.println(String.format("Salario pretendido: %.2f", salarioPretendido[i]));  
+        }
+
+        int contador=0;
+        for (int i=0; i <=9; i++){ //verificacao para nao ultrapassar 5 pessoas
+            if (salarioPretendido[i] <= 2000 && contador <5){
+                System.out.println("Vaga Garantida para - " + candidatos[i]);
+                contador +=1;
+            } 
+        }
+    }
+    
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+}
+}
---
+++ src/edu/caue/cases/testeArrayDouble.java
@@ -0,0 +1,20 @@
+package edu.caue.cases;
+import java.util.concurrent.ThreadLocalRandom;
+public class testeArrayDouble {
+    
+    public static void main(String[] args) {
+        double [] salarios = new double[10];
+        
+        System.out.println(salarios.length);
+            for (int i=0; i <=9 ; i++){
+                salarios[i] = valorPretendido();
+                System.out.println(salarios[i]); 
+            }
+    }
+    
+   
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+    
+}
