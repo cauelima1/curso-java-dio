50c88e2799d331b6a1c6a3e58c15236d19a0911c
cauelima1 <cauezak@gmail.com>
2024-07-15 13:45:40 -0300

    ajustes de codigo

--- src/edu/caue/cases/ProcessoSeletivo2.java
+++ src/edu/caue/cases/ProcessoSeletivo2.java
@@ -1,33 +1,44 @@
 package edu.caue.cases;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo2 {
+
+   
+
     public static void main(String[] args) {
-        
+
+        ImprimirCandidadoseSelecionados();
+   
+    }
+                      
+    static void ImprimirCandidadoseSelecionados () {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
         double [] salarioPretendido = new double[10];
-      
-        
-        System.out.println("Temos " + candidatos.length + " candidatos:");
-       
-        
-        for (int i=0; i <=9; i++) {
-            System.out.println(i+1 + " - " + candidatos[i]);
-            salarioPretendido[i] = valorPretendido();
-            System.out.println(String.format("Salario pretendido: %.2f", salarioPretendido[i]));  
-        }
+           for (int i=0; i <=9; i++) {
+                salarioPretendido[i] = valorPretendido();
+                if (salarioPretendido[i] <= 2000.0){
+                    System.out.println(i+1 + " - " + candidatos[i] + " - Solicitou este valor de salario:  R$" + salarioPretendido[i] + " e foi SELECIONADO PARA VAGA.");
+                   
+                } else
+                System.out.println(i+1 + " - " + candidatos[i] + " - Solicitou este valor de salario:  R$" + salarioPretendido[i]);      
+            }
+            System.out.println("--------------------------------");
+            int i=0;
+            int x=0;
+            while(x < 5 && i<=9){
+                    if (salarioPretendido[i] <= 2000.0){
+                        System.out.println(candidatos[i] + " foi selecionado para a vaga.");
+                        x++;
+                    }
+                i++;
+            }
+           
+    }    
 
-        int contador=0;
-        for (int i=0; i <=9; i++){ //verificacao para nao ultrapassar 5 pessoas.
-            if (salarioPretendido[i] <= 2000 && contador <5){
-                System.out.println("Vaga Garantida para - " + candidatos[i]);
-                contador +=1;
-            } 
-        }
-    }
-    
+
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
-}
+    }
 }
